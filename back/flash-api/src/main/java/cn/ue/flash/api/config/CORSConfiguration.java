ad3d751d1cf69f1eb844aa990720b3a2b8977534
masonleung2016 <masonleung@163.com>
2020-07-20 21:00:32 +0800

    Create CORSConfiguration.java

---
+++ back/flash-api/src/main/java/cn/ue/flash/api/config/CORSConfiguration.java
@@ -0,0 +1,31 @@
+package cn.ue.flash.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Description: 跨域配置
+ * @Author: LCF
+ * @Date: 2020/7/20 13:20
+ * @Package: cn.ue.flash.api.config
+ * @Version 1.0
+ */
+
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
