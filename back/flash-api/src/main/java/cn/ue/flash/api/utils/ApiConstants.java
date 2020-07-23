f29eea4ab727e982ce35aeaec9536954bc7a21d4
masonleung2016 <masonleung@163.com>
2020-07-23 15:55:44 +0800

    Create ApiConstants.java

---
+++ back/flash-api/src/main/java/cn/ue/flash/api/utils/ApiConstants.java
@@ -0,0 +1,21 @@
+package cn.ue.flash.api.utils;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 13:42
+ * @Package: cn.ue.flash.api.utils
+ * @Version 1.0
+ */
+
+public class ApiConstants {
+    public static final String HTTP_METHOD_OPTIONS = "OPTIONS";
+
+    public static final String ADMIN_ACCOUNT = "admin";
+
+    public static final String IP_UNKNOWN = "unknown";
+
+    public static final String IPV6_LOCALHOST = "0:0:0:0:0:0:0:1";
+
+    public static final String IPV4_LOCALHOST = "127.0.0.1";
+}
