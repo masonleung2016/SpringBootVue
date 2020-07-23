2319ec5f084361a62d21d01e2731bd3539d43feb
masonleung2016 <masonleung@163.com>
2020-07-23 15:52:49 +0800

    Create PackageInfo.java

---
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/PackageInfo.java
@@ -0,0 +1,12 @@
+package cn.ue.flash.api.controller;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 13:39
+ * @Package: cn.ue.flash.api.controller
+ * @Version 1.0
+ */
+
+public class PackageInfo {
+}
