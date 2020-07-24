c16a66d8ed64a6041c381300512eaef26061ae6e
masonleung2016 <masonleung@163.com>
2020-07-24 09:24:05 +0800

    Create CacheKey.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/cache/CacheKey.java
@@ -0,0 +1,41 @@
+package cn.ue.flash.bean.constant.cache;
+
+/**
+ * @Description: 缓存的key集合
+ * @Author: LCF
+ * @Date: 2020/7/20 13:47
+ * @Package: cn.ue.flash.bean.constant.cache
+ * @Version 1.0
+ */
+
+public interface CacheKey {
+
+    /**
+     * ConstantFactory中的缓存
+     */
+    String ROLES_NAME = "roles_name_";
+
+    String SINGLE_ROLE_NAME = "single_role_name_";
+
+    String SINGLE_ROLE_TIP = "single_role_tip_";
+
+    String DEPT_NAME = "dept_name_";
+
+    String DICT_NAME = "dict_name_";
+
+    String DICT = "dict_";
+
+    String COURSE_NAME = "course_name_";
+
+    String ORG_NAME = "org_name_";
+
+    String DIC_ALL = "dic_all_";
+
+    String CFG = "cfg_";
+
+    String MENU_NAME = "menu_name_";
+
+    String SYS_USER_NAME = "SYS_USER_NAME";
+
+    String FILE_INFO = "file_info";
+}
