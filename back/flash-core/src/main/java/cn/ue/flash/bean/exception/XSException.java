626221e9baafe943868957f8942ba1fe3cc281cd
masonleung2016 <masonleung@163.com>
2020-07-24 11:54:22 +0800

    Create XSException.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/exception/XSException.java
@@ -0,0 +1,30 @@
+package cn.ue.flash.bean.exception;
+
+/**
+ * @Description: 定义通用异常。code 存储异常代码
+ * @Author: LCF
+ * @Date: 2020/7/20 14:20
+ * @Package: cn.ue.flash.bean.exception
+ * @Version 1.0
+ */
+
+public class XSException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    private String code;
+
+    public XSException(String code, String message, Throwable t) {
+        super(message, t);
+        this.code = code;
+    }
+
+    public XSException(String code, String message) {
+        this(code, message, null);
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+}
