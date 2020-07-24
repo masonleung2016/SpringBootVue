16c846094d31105b0019abc9277341b56197e30e
masonleung2016 <masonleung@163.com>
2020-07-24 17:16:18 +0800

    Create ContactsRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/cms/ContactsRepository.java
@@ -0,0 +1,15 @@
+package cn.ue.flash.dao.cms;
+
+import cn.ue.flash.bean.entity.cms.Contacts;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:48
+ * @Package: cn.ue.flash.dao.cms
+ * @Version 1.0
+ */
+
+public interface ContactsRepository extends BaseRepository<Contacts, Long> {
+}
