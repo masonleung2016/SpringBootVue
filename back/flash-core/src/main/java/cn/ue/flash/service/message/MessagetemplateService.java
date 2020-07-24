1e64b8815dd5d99c83be35d5d628ee322e294c0a
masonleung2016 <masonleung@163.com>
2020-07-24 17:29:05 +0800

    Create MessagetemplateService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/message/MessagetemplateService.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.service.message;
+
+import cn.ue.flash.bean.entity.message.MessageTemplate;
+import cn.ue.flash.dao.message.MessagetemplateRepository;
+import cn.ue.flash.service.BaseService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:08
+ * @Package: cn.ue.flash.service.message
+ * @Version 1.0
+ */
+
+@Service
+public class MessagetemplateService extends BaseService<MessageTemplate, Long, MessagetemplateRepository> {
+
+}
