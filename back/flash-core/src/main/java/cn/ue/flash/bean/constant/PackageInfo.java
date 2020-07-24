0a6737df2a17920fc7f2ba4566a743d9767e2d2e
masonleung2016 <masonleung@163.com>
2020-07-24 09:28:45 +0800

    Create PackageInfo.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/PackageInfo.java
@@ -0,0 +1,12 @@
+package cn.ue.flash.bean.constant;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 13:52
+ * @Package: cn.ue.flash.bean.constant
+ * @Version 1.0
+ */
+
+public class PackageInfo {
+}
