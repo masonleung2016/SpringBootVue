b935f78b543ad010b754edf9406ac5464304ae1f
masonleung2016 <masonleung@163.com>
2020-07-24 09:27:30 +0800

    Create Order.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/state/Order.java
@@ -0,0 +1,28 @@
+package cn.ue.flash.bean.constant.state;
+
+/**
+ * @Description: 数据库排序
+ * @Author: LCF
+ * @Date: 2020/7/20 13:51
+ * @Package: cn.ue.flash.bean.constant.state
+ * @Version 1.0
+ */
+
+public enum Order {
+
+    ASC("asc"), DESC("desc");
+
+    private String des;
+
+    Order(String des) {
+        this.des = des;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+}
