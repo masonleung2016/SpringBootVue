c3442b08e2f0afd41b44fca43a1c323e6ebb2efe
masonleung2016 <masonleung@163.com>
2020-07-24 09:32:54 +0800

    Create MessageTemplate.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/core/entity/message/MessageTemplate.java
@@ -0,0 +1,23 @@
+package cn.ue.flash.bean.entity.message;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:00
+ * @Package: cn.ue.flash.bean.entity.message
+ * @Version 1.0
+ */
+
+public class MessageTemplate {
+    public boolean getId() {
+        return false;
+    }
+
+    public String getContent() {
+        return null;
+    }
+
+    public Long getIdMessageSender() {
+        return null;
+    }
+}
