d1b722af9dc1f11e5058742ec9f881de7505f6ed
masonleung2016 <masonleung@163.com>
2020-07-24 17:16:56 +0800

    Create MessagesenderRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/message/MessagesenderRepository.java
@@ -0,0 +1,15 @@
+package cn.ue.flash.dao.message;
+
+import cn.ue.flash.bean.entity.message.MessageSender;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:49
+ * @Package: cn.ue.flash.dao.message
+ * @Version 1.0
+ */
+
+public interface MessagesenderRepository extends BaseRepository<MessageSender, Long> {
+}
