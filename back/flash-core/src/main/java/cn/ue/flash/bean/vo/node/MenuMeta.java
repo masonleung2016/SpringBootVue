61a00ca084df1fea0d6a45055892e12a8a108f60
masonleung2016 <masonleung@163.com>
2020-07-24 14:06:34 +0800

    Create MenuMeta.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/vo/node/MenuMeta.java
@@ -0,0 +1,25 @@
+package cn.ue.flash.bean.vo.node;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:24
+ * @Package: cn.ue.flash.bean.vo.node
+ * @Version 1.0
+ */
+
+@Data
+public class MenuMeta {
+    private String title;
+    private String icon;
+
+    public void setIcon(String valueOf) {
+
+    }
+
+    public void setTitle(String valueOf) {
+
+    }
+}
