d8cd7a96d4489780f5eb686dfe326558ba7d951f
masonleung2016 <masonleung@163.com>
2020-07-24 14:09:09 +0800

    Create ArticleVo.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/vo/offcialsite/ArticleVo.java
@@ -0,0 +1,35 @@
+package cn.ue.flash.bean.vo.offcialsite;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:30
+ * @Package: cn.ue.flash.bean.vo.offcialsite
+ * @Version 1.0
+ */
+
+@Data
+public class ArticleVo {
+    private String id;
+    private String title;
+    private String content;
+    private Author author;
+    private Date createAt;
+    private List<Reply> replies;
+
+    public ArticleVo() {
+    }
+
+    public ArticleVo(String id, String title, String content, Author author, Date createAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createAt = createAt;
+    }
+}
