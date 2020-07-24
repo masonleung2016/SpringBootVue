94337b5266efa711cb84cd2b45968ea506a741d3
masonleung2016 <masonleung@163.com>
2020-07-24 17:45:35 +0800

    Create Constants.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/utils/Constants.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.utils;
+
+/**
+ * @Description: 系统常量
+ * @Author: LCF
+ * @Date: 2020/7/20 15:36
+ * @Package: cn.ue.flash.utils
+ * @Version 1.0
+ */
+
+public interface Constants {
+    /**
+     * 默认的系统用户id
+     */
+    long SYSTEM_USER_ID = -1;
+
+
+    String SEPARATOR = ";;;";
+}
