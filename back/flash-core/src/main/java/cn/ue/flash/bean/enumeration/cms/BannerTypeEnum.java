9ee4c06e42de0828f7512fe393a1661e1985726f
masonleung2016 <masonleung@163.com>
2020-07-24 09:38:59 +0800

    Create BannerTypeEnum.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/enumeration/cms/BannerTypeEnum.java
@@ -0,0 +1,28 @@
+package cn.ue.flash.bean.enumeration.cms;
+
+/**
+ * @Description: 广告类型
+ * @Author: LCF
+ * @Date: 2020/7/20 14:09
+ * @Package: cn.ue.flash.bean.enumeration.cms
+ * @Version 1.0
+ */
+
+public enum BannerTypeEnum {
+
+    CASE("case"),
+    INDEX("index"),
+    NEWS("news"),
+    PRODUCT("product"),
+    SOLUTION("solution");
+
+    private String value;
+
+    BannerTypeEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
