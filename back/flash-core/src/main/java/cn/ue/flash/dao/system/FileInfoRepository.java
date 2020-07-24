ec4ac2c0e2dfc93d9af03b37e7f4f2cd79d6b61a
masonleung2016 <masonleung@163.com>
2020-07-24 17:18:14 +0800

    Create FileInfoRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/system/FileInfoRepository.java
@@ -0,0 +1,15 @@
+package cn.ue.flash.dao.system;
+
+import cn.ue.flash.bean.entity.system.FileInfo;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:51
+ * @Package: cn.ue.flash.dao.system
+ * @Version 1.0
+ */
+
+public interface FileInfoRepository extends BaseRepository<FileInfo, Long> {
+}
