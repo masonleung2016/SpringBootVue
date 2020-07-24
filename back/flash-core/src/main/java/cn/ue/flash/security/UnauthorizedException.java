9e630a4da22238658328b46de990c30634e90b8c
masonleung2016 <masonleung@163.com>
2020-07-24 17:24:25 +0800

    Create UnauthorizedException.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/security/UnauthorizedException.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.security;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:01
+ * @Package: cn.ue.flash.security
+ * @Version 1.0
+ */
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+
+    public UnauthorizedException() {
+        super();
+    }
+}
