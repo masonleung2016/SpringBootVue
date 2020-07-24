e22b9182ca42429fd9f210c2de4b002e0a88173b
masonleung2016 <masonleung@163.com>
2020-07-24 10:00:27 +0800

    Create ChannelEnum.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/enumeration/cms/ChannelEnum.java
@@ -0,0 +1,34 @@
+package cn.ue.flash.bean.enumeration.cms;
+
+/**
+ * @Description: 频道类型
+ * @Author: LCF
+ * @Date: 2020/7/20 14:10
+ * @Package: cn.ue.flash.bean.enumeration.cms
+ * @Version 1.0
+ */
+
+public enum ChannelEnum {
+
+    CASE(4L, "case"),
+    NEWS(1L, "news"),
+    PRODUCT(2L, "product"),
+    SOLUTION(3L, "solution");
+
+
+    private String value;
+    private Long id;
+
+    ChannelEnum(Long id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
