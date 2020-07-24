10066a56d6cb3e1e555f60a81df0025eefc5f33d
masonleung2016 <masonleung@163.com>
2020-07-24 17:16:43 +0800

    Create MessageRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/message/MessageRepository.java
@@ -0,0 +1,18 @@
+package cn.ue.flash.dao.message;
+
+import cn.ue.flash.bean.entity.message.Message;
+import cn.ue.flash.dao.BaseRepository;
+
+import java.util.ArrayList;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:48
+ * @Package: cn.ue.flash.dao.message
+ * @Version 1.0
+ */
+
+public interface MessageRepository extends BaseRepository<Message, Long> {
+    void deleteAllByIdIn(ArrayList<String> list);
+}
