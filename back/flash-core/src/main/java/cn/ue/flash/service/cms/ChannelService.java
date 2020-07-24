055ebb3f52051ee64462b22b429cce5b3112dad4
masonleung2016 <masonleung@163.com>
2020-07-24 17:25:23 +0800

    Create ChannelService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/cms/ChannelService.java
@@ -0,0 +1,18 @@
+package cn.ue.flash.service.cms;
+
+import cn.ue.flash.bean.entity.cms.Channel;
+import cn.ue.flash.dao.cms.ChannelRepository;
+import cn.ue.flash.service.BaseService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 文章频道service
+ * @Author: LCF
+ * @Date: 2020/7/20 15:03
+ * @Package: cn.ue.flash.service.cms
+ * @Version 1.0
+ */
+
+@Service
+public class ChannelService extends BaseService<Channel, Long, ChannelRepository> {
+}
