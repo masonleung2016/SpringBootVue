79366a42a1f18e2733761a21d3c094a7cd6b830f
masonleung2016 <masonleung@163.com>
2020-07-24 14:32:16 +0800

    Create DictFieldWarpperFactory.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/core/factory/DictFieldWarpperFactory.java
@@ -0,0 +1,43 @@
+package cn.ue.flash.core.factory;
+
+import cn.ue.flash.bean.enumeration.BizExceptionEnum;
+import cn.ue.flash.bean.exception.ApplicationException;
+import cn.ue.flash.service.system.IConstantFactory;
+import cn.ue.flash.service.system.impl.ConstantFactory;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Description: 字段的包装创建工厂
+ * @Author: LCF
+ * @Date: 2020/7/20 14:42
+ * @Package: cn.ue.flash.core.factory
+ * @Version 1.0
+ */
+
+public class DictFieldWarpperFactory {
+
+    public static Object createFieldWarpper(Object field, String methodName) {
+        IConstantFactory me = ConstantFactory.me();
+        try {
+            Method method = IConstantFactory.class.getMethod(methodName, field.getClass());
+            Object result = method.invoke(me, field);
+            return result;
+        } catch (Exception e) {
+            try {
+                Method method = IConstantFactory.class.getMethod(methodName, Long.class);
+                Object result = method.invoke(me, Long.valueOf(field.toString()));
+                return result;
+            } catch (Exception e1) {
+                try {
+                    Method method = IConstantFactory.class.getMethod(methodName, Integer.class);
+                    Object result = method.invoke(me, Integer.parseInt(field.toString()));
+                    return result;
+                } catch (Exception e2) {
+                    throw new ApplicationException(BizExceptionEnum.ERROR_WRAPPER_FIELD);
+                }
+            }
+        }
+    }
+
+}
