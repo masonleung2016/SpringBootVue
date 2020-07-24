0e35541c271f96236a15ac3db5c073321eab3518
masonleung2016 <masonleung@163.com>
2020-07-24 17:42:19 +0800

    Create UpdateService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/UpdateService.java
@@ -0,0 +1,18 @@
+package cn.ue.flash.service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:29
+ * @Package: cn.ue.flash.service
+ * @Version 1.0
+ */
+
+public interface UpdateService<T, ID> {
+    /**
+     * 修改记录信息
+     * @param record 要修改的对象
+     * @return 返回修改的记录
+     */
+    T update(T record);
+}
