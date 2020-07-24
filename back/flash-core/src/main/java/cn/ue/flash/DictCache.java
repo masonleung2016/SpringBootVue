99daef4749231625432ce7aef98be2393e2984b7
masonleung2016 <masonleung@163.com>
2020-07-24 14:24:19 +0800

    Create DictCache.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/DictCache.java
@@ -0,0 +1,20 @@
+package cn.ue.flash.cache;
+
+import cn.ue.flash.bean.entity.system.Dict;
+
+import java.util.List;
+
+/**
+ * @Description: 字典缓存
+ * @Author: LCF
+ * @Date: 2020/7/20 14:41
+ * @Package: cn.ue.flash.cache
+ * @Version 1.0
+ */
+
+public interface DictCache extends Cache {
+
+    List<Dict> getDictsByPname(String dictName);
+
+    String getDict(Long dictId);
+}
