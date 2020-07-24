3445677f0511889747fda713bfcf1f9f6f3885c1
masonleung2016 <masonleung@163.com>
2020-07-24 17:23:28 +0800

    Create JwtToken.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/security/JwtToken.java
@@ -0,0 +1,33 @@
+package cn.ue.flash.security;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:58
+ * @Package: cn.ue.flash.security
+ * @Version 1.0
+ */
+
+public class JwtToken implements AuthenticationToken {
+
+    /**
+     * 密钥
+     */
+    private String token;
+
+    public JwtToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+}
