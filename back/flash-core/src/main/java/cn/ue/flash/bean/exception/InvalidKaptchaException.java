d421f21aa51bba41f0ae7972a22429d7b3008a64
masonleung2016 <masonleung@163.com>
2020-07-24 11:52:28 +0800

    Create InvalidKaptchaException.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/exception/InvalidKaptchaException.java
@@ -0,0 +1,12 @@
+package cn.ue.flash.bean.exception;
+
+/**
+ * @Description: 验证码错误异常
+ * @Author: LCF
+ * @Date: 2020/7/20 14:18
+ * @Package: cn.ue.flash.bean.exception
+ * @Version 1.0
+ */
+
+public class InvalidKaptchaException extends RuntimeException {
+}
