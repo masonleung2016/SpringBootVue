73c840cd86d7057c4342845c2819f814f43aa37f
masonleung2016 <masonleung@163.com>
2020-07-24 12:04:52 +0800

    Create Rets.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/vo/front/Rets.java
@@ -0,0 +1,32 @@
+package cn.ue.flash.bean.vo.front;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:22
+ * @Package: cn.ue.flash.bean.vo.front
+ * @Version 1.0
+ */
+
+public class Rets {
+
+    public static final Integer SUCCESS = 20000;
+    public static final Integer FAILURE = 9999;
+    public static final Integer TOKEN_EXPIRE = 50014;
+
+    public static Ret success(Object data) {
+        return new Ret(Rets.SUCCESS, "成功", data);
+    }
+
+    public static Ret failure(String msg) {
+        return new Ret(Rets.FAILURE, msg, null);
+    }
+
+    public static Ret success() {
+        return new Ret(Rets.SUCCESS, "成功", null);
+    }
+
+    public static Ret expire() {
+        return new Ret(Rets.TOKEN_EXPIRE, "token 过期", null);
+    }
+}
