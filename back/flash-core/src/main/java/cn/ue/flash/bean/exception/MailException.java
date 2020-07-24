e198625dd936b8bf29c01785758f5c571d5527fa
masonleung2016 <masonleung@163.com>
2020-07-24 11:52:41 +0800

    Create MailException.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/exception/MailException.java
@@ -0,0 +1,20 @@
+package cn.ue.flash.bean.exception;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:18
+ * @Package: cn.ue.flash.bean.exception
+ * @Version 1.0
+ */
+
+public class MailException extends Exception {
+
+    public MailException(String msg) {
+        super(msg);
+    }
+
+    public MailException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
