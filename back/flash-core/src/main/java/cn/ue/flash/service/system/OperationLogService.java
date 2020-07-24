401c16ef4809479acf882e239cef52f389062bbf
masonleung2016 <masonleung@163.com>
2020-07-24 17:34:04 +0800

    Create OperationLogService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/system/OperationLogService.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.service.system;
+
+import cn.ue.flash.bean.entity.system.OperationLog;
+import cn.ue.flash.dao.system.OperationLogRepository;
+import cn.ue.flash.service.BaseService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:18
+ * @Package: cn.ue.flash.service.system
+ * @Version 1.0
+ */
+
+@Service
+public class OperationLogService extends BaseService<OperationLog, Long, OperationLogRepository> {
+
+}
