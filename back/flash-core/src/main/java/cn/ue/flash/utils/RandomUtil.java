fc1204dfdb0c594afcde657f0621586334867f47
masonleung2016 <masonleung@163.com>
2020-07-24 17:57:47 +0800

    Create RandomUtil.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/utils/RandomUtil.java
@@ -0,0 +1,24 @@
+package cn.ue.flash.utils;
+
+import java.util.Random;
+
+/**
+ * @Description: 获取随机位数的字符串
+ * @Author: LCF
+ * @Date: 2020/7/20 15:46
+ * @Package: cn.ue.flash.utils
+ * @Version 1.0
+ */
+
+public class RandomUtil {
+    public static String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+}
