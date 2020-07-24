36ef962cd4f5d1428b832a8d3b5e5a04936771ff
masonleung2016 <masonleung@163.com>
2020-07-24 17:37:52 +0800

    Create BoyService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/test/BoyService.java
@@ -0,0 +1,26 @@
+package cn.ue.flash.service.test;
+
+import cn.ue.flash.bean.entity.test.Boy;
+import cn.ue.flash.dao.test.BoyRepository;
+
+import cn.ue.flash.service.BaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:25
+ * @Package: cn.ue.flash.service.test
+ * @Version 1.0
+ */
+
+@Service
+public class BoyService extends BaseService<Boy, Long, BoyRepository> {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private BoyRepository boyRepository;
+
+}
