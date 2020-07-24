eeafb9b0e3714343328c6f87f56eed8f963d82c2
masonleung2016 <masonleung@163.com>
2020-07-24 09:23:33 +0800

    Create Cache.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/cache/Cache.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.bean.constant.cache;
+
+/**
+ * @Description: 所有缓存名称的集合
+ * @Author: LCF
+ * @Date: 2020/7/20 13:46
+ * @Package: cn.ue.flash.bean.constant.cache
+ * @Version 1.0
+ */
+
+public interface Cache {
+
+    /**
+     * 常量缓存
+     */
+    String CONSTANT = "CONSTANT";
+
+    String APPLICATION = "APPLICATION";
+}
