ded166324eb58603f4d851e3999c7781b3085871
masonleung2016 <masonleung@163.com>
2020-07-24 17:36:42 +0800

    Create QuartzConfigration.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/task/QuartzConfigration.java
@@ -0,0 +1,23 @@
+package cn.ue.flash.service.task;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:24
+ * @Package: cn.ue.flash.service.task
+ * @Version 1.0
+ */
+
+@Configuration
+public class QuartzConfigration {
+
+    @Bean(name = "scheduler")
+    public SchedulerFactoryBean schedulerFactory() {
+        SchedulerFactoryBean bean = new SchedulerFactoryBean();
+        return bean;
+    }
+}
