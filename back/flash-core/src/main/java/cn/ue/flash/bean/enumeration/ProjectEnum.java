351d72bca65ba45a87f5a41412b8f61975ac42fa
masonleung2016 <masonleung@163.com>
2020-07-24 10:02:26 +0800

    Create ProjectEnum.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/enumeration/ProjectEnum.java
@@ -0,0 +1,26 @@
+package cn.ue.flash.bean.enumeration;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:14
+ * @Package: cn.ue.flash.bean.enumeration
+ * @Version 1.0
+ */
+
+public enum ProjectEnum {
+
+    DOLPHIN_PROD("dolphin"),
+    SCORE_CARD("ScoreCard"),
+    SNOW_PRODUCT("PRJ_SNOW_PRODUCT");
+
+    private String value;
+
+    ProjectEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
