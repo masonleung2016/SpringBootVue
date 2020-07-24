338cda9a12539ff484bc37e613def6db8aa3ac3a
masonleung2016 <masonleung@163.com>
2020-07-24 18:00:53 +0800

    Create RoleWarpper.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/warpper/RoleWarpper.java
@@ -0,0 +1,28 @@
+package cn.ue.flash.warpper;
+
+import cn.ue.flash.service.system.impl.ConstantFactory;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: 角色列表的包装类
+ * @Author: LCF
+ * @Date: 2020/7/20 15:56
+ * @Package: cn.ue.flash.warpper
+ * @Version 1.0
+ */
+
+public class RoleWarpper extends BaseControllerWarpper {
+
+    public RoleWarpper(List<Map<String, Object>> list) {
+        super(list);
+    }
+
+    @Override
+    public void warpTheMap(Map<String, Object> map) {
+        map.put("pName", ConstantFactory.me().getSingleRoleName((Long) map.get("pid")));
+        map.put("deptName", ConstantFactory.me().getDeptName((Long) map.get("deptid")));
+    }
+
+}
