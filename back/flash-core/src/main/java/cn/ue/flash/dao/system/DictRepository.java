30bca1b1c09ed8e756de0e41dd18f75a4e3db6fc
masonleung2016 <masonleung@163.com>
2020-07-24 17:17:58 +0800

    Create DictRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/system/DictRepository.java
@@ -0,0 +1,22 @@
+package cn.ue.flash.dao.system;
+
+import cn.ue.flash.bean.entity.system.Dict;
+import cn.ue.flash.dao.BaseRepository;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:50
+ * @Package: cn.ue.flash.dao.system
+ * @Version 1.0
+ */
+
+public interface DictRepository extends BaseRepository<Dict, Long> {
+    List<Dict> findByPid(Long pid);
+
+    List<Dict> findByNameAndPid(String name, Long pid);
+
+    List<Dict> findByNameLike(String name);
+}
