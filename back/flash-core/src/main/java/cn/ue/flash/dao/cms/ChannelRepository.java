f77db81fd72444180656735fe3554c93c4e5e04b
masonleung2016 <masonleung@163.com>
2020-07-24 17:15:58 +0800

    Create ChannelRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/cms/ChannelRepository.java
@@ -0,0 +1,15 @@
+package cn.ue.flash.dao.cms;
+
+import cn.ue.flash.bean.entity.cms.Channel;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:47
+ * @Package: cn.ue.flash.dao.cms
+ * @Version 1.0
+ */
+
+public interface ChannelRepository extends BaseRepository<Channel, Long> {
+}
