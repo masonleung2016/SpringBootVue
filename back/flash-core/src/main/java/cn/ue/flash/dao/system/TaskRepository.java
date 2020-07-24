12c0454aacd13251d3af1afdf88bb9b26fe8858a
masonleung2016 <masonleung@163.com>
2020-07-24 17:20:03 +0800

    Create TaskRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/system/TaskRepository.java
@@ -0,0 +1,24 @@
+package cn.ue.flash.dao.system;
+
+import cn.ue.flash.bean.entity.system.Task;
+import cn.ue.flash.dao.BaseRepository;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:53
+ * @Package: cn.ue.flash.dao.system
+ * @Version 1.0
+ */
+
+public interface TaskRepository extends BaseRepository<Task, Long> {
+
+    long countByNameLike(String name);
+
+    List<Task> findByNameLike(String name);
+
+    List<Task> findAllByDisabled(boolean disable);
+}
+
