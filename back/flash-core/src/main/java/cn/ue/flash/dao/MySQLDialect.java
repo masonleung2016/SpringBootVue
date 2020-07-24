e9a989e27464eb5b61084d69d997624cbcdc4f8a
masonleung2016 <masonleung@163.com>
2020-07-24 17:22:45 +0800

    Create MySQLDialect.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/MySQLDialect.java
@@ -0,0 +1,20 @@
+package cn.ue.flash.dao;
+
+import org.hibernate.dialect.InnoDBStorageEngine;
+import org.hibernate.dialect.MySQL5Dialect;
+import org.hibernate.dialect.MySQLStorageEngine;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:57
+ * @Package: cn.ue.flash.dao
+ * @Version 1.0
+ */
+
+public class MySQLDialect extends MySQL5Dialect {
+    @Override
+    protected MySQLStorageEngine getDefaultMySQLStorageEngine() {
+        return InnoDBStorageEngine.INSTANCE;
+    }
+}
