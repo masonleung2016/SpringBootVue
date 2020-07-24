82d4855f82925cb8dff58f52b1456d1861e6d218
masonleung2016 <masonleung@163.com>
2020-07-24 11:54:02 +0800

    Create ValidException.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/exception/ValidException.java
@@ -0,0 +1,20 @@
+package cn.ue.flash.bean.exception;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:20
+ * @Package: cn.ue.flash.bean.exception
+ * @Version 1.0
+ */
+
+public class ValidException extends RuntimeException {
+
+    public ValidException(String msg) {
+        super(msg);
+    }
+
+    public ValidException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
