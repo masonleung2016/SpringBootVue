2af994f3b15928eae18c7901b187d5c42bfef9f1
masonleung2016 <masonleung@163.com>
2020-07-24 14:09:42 +0800

    Create Author.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/vo/offcialsite/Author.java
@@ -0,0 +1,18 @@
+package cn.ue.flash.bean.vo.offcialsite;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:30
+ * @Package: cn.ue.flash.bean.vo.offcialsite
+ * @Version 1.0
+ */
+
+@Data
+public class Author {
+    private String name;
+    private String avatar;
+
+}
