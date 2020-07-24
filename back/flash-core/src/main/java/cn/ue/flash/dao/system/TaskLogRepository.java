e0dca30fad2319615792f8688524250b25371940
masonleung2016 <masonleung@163.com>
2020-07-24 17:19:51 +0800

    Create TaskLogRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/system/TaskLogRepository.java
@@ -0,0 +1,16 @@
+package cn.ue.flash.dao.system;
+
+import cn.ue.flash.bean.entity.system.TaskLog;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:53
+ * @Package: cn.ue.flash.dao.system
+ * @Version 1.0
+ */
+
+public interface TaskLogRepository extends BaseRepository<TaskLog, Long> {
+
+}
