9d0b2fd65eebff420f6b0015c10de8cf203a12e2
masonleung2016 <masonleung@163.com>
2020-07-24 17:45:57 +0800

    Create CryptUtil.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/utils/CryptUtil.java
@@ -0,0 +1,23 @@
+package cn.ue.flash.utils;
+
+import java.util.Base64;
+
+/**
+ * @Description: 加密工具类
+ * @Author: LCF
+ * @Date: 2020/7/20 15:37
+ * @Package: cn.ue.flash.utils
+ * @Version 1.0
+ */
+
+public class CryptUtil {
+
+    public static String encodebase64(byte[] bytes) {
+
+        String encode = Base64.getEncoder().encodeToString(bytes);
+        encode = encode.replaceAll("\n", "");
+        return encode;
+    }
+
+
+}
