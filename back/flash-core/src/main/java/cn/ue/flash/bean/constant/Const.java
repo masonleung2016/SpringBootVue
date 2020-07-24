7de601e0f09c5afeb0f13d941ae359759553d8f4
masonleung2016 <masonleung@163.com>
2020-07-24 09:28:16 +0800

    Create Const.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/Const.java
@@ -0,0 +1,38 @@
+package cn.ue.flash.bean.constant;
+
+/**
+ * @Description: 系统常量
+ * @Author: LCF
+ * @Date: 2020/7/20 13:51
+ * @Package: cn.ue.flash.bean.constant
+ * @Version 1.0
+ */
+
+public interface Const {
+
+    /**
+     * 系统默认的管理员密码
+     */
+    String DEFAULT_PWD = "111111";
+
+    /**
+     * 管理员角色的名字
+     */
+    String ADMIN_NAME = "administrator";
+
+    /**
+     * 管理员id
+     */
+    Integer ADMIN_ID = 1;
+
+    /**
+     * 超级管理员角色id
+     */
+    Integer ADMIN_ROLE_ID = 1;
+
+    /**
+     * 接口文档的菜单名
+     */
+    String API_MENU_NAME = "接口文档";
+
+}
