0f0cce115a0fbe4221f2ede04ff68eb7d6bbea44
masonleung2016 <masonleung@163.com>
2020-07-24 17:40:10 +0800

    Create CrudService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/CrudService.java
@@ -0,0 +1,16 @@
+package cn.ue.flash.service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:27
+ * @Package: cn.ue.flash.service
+ * @Version 1.0
+ */
+
+public interface CrudService<T, ID> extends
+        InsertService<T, ID>,
+        UpdateService<T, ID>,
+        DeleteService<ID>,
+        SelectService<T, ID> {
+}
