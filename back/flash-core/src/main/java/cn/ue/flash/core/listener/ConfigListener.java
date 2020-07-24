7df31d94c5768f9e3e81d541904e17003e22c07f
masonleung2016 <masonleung@163.com>
2020-07-24 14:33:51 +0800

    Create ConfigListener.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/core/listener/ConfigListener.java
@@ -0,0 +1,38 @@
+package cn.ue.flash.core.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:44
+ * @Package: cn.ue.flash.core.listener
+ * @Version 1.0
+ */
+
+public class ConfigListener implements ServletContextListener {
+
+    private static Map<String, String> conf = new HashMap<>();
+
+    public static Map<String, String> getConf() {
+        return conf;
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent arg0) {
+        conf.clear();
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent evt) {
+        ServletContext sc = evt.getServletContext();
+        /** 项目路径*/
+        conf.put("realPath", sc.getRealPath("/").replaceFirst("/", ""));
+        conf.put("contextPath", sc.getContextPath());
+    }
+
+}
