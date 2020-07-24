0cb56a3ec3a0268d9641cbc00e520c0c3a72e4e8
masonleung2016 <masonleung@163.com>
2020-07-24 17:20:59 +0800

    Create BoyRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/test/BoyRepository.java
@@ -0,0 +1,16 @@
+package cn.ue.flash.dao.test;
+
+import cn.ue.flash.bean.entity.test.Boy;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:54
+ * @Package: cn.ue.flash.dao.test
+ * @Version 1.0
+ */
+
+public interface BoyRepository extends BaseRepository<Boy, Long> {
+
+}
