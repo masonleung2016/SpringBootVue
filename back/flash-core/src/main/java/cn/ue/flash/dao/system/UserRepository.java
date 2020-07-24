1ccca87a5863333aa14afd5219b98a6225b9a8f3
masonleung2016 <masonleung@163.com>
2020-07-24 17:20:15 +0800

    Create UserRepository.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/dao/system/UserRepository.java
@@ -0,0 +1,18 @@
+package cn.ue.flash.dao.system;
+
+import cn.ue.flash.bean.entity.system.User;
+import cn.ue.flash.dao.BaseRepository;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:54
+ * @Package: cn.ue.flash.dao.system
+ * @Version 1.0
+ */
+
+public interface UserRepository extends BaseRepository<User, Long> {
+    User findByAccount(String account);
+
+    User findByAccountAndStatusNot(String account, Integer status);
+}
