bbcae3f983e64a7299f3cfee07dfbf6dfcfcc2b9
masonleung2016 <masonleung@163.com>
2020-07-24 17:40:42 +0800

    Create InsertService.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/service/InsertService.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.service;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:28
+ * @Package: cn.ue.flash.service
+ * @Version 1.0
+ */
+
+public interface InsertService<T, ID> {
+
+    /**
+     * 添加一条数据
+     * @param record 要添加的数据
+     * @return 添加后生成的主键
+     */
+    T insert(T record);
+}
