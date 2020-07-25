a3db11cb8aabbfb23656dbeed84ed0e32e678585
masonleung2016 <masonleung@163.com>
2020-07-25 14:19:44 +0800

    Create CodeConfig.java

---
+++ back/flash-generator/src/main/java/cn/ue/flash/code/CodeConfig.java
@@ -0,0 +1,75 @@
+package cn.ue.flash.code;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 15:58
+ * @Package: cn.ue.flash.code
+ * @Version 1.0
+ */
+
+
+public class CodeConfig {
+    private String entityModel = "flash-core";
+    private String daoModel = "flash-core";
+    private String serviceModel = "flash-core";
+    private String controllerModel = "flash-api";
+    private String viewModel = "flash-vue-admin";
+
+    public String getModel(String type) {
+        switch (type) {
+            case "model":
+                return entityModel;
+            case "repository":
+                return daoModel;
+            case "service":
+                return serviceModel;
+            case "controller":
+                return controllerModel;
+            case "view":
+                return viewModel;
+            default:
+                return null;
+        }
+    }
+
+    public String getEntityModel() {
+        return entityModel;
+    }
+
+    public void setEntityModel(String entityModel) {
+        this.entityModel = entityModel;
+    }
+
+    public String getDaoModel() {
+        return daoModel;
+    }
+
+    public void setDaoModel(String daoModel) {
+        this.daoModel = daoModel;
+    }
+
+    public String getServiceModel() {
+        return serviceModel;
+    }
+
+    public void setServiceModel(String serviceModel) {
+        this.serviceModel = serviceModel;
+    }
+
+    public String getControllerModel() {
+        return controllerModel;
+    }
+
+    public void setControllerModel(String controllerModel) {
+        this.controllerModel = controllerModel;
+    }
+
+    public String getViewModel() {
+        return viewModel;
+    }
+
+    public void setViewModel(String viewModel) {
+        this.viewModel = viewModel;
+    }
+}
