e6a9fde73666f2a67af4137f8cd63fc148e1ee7d
masonleung2016 <masonleung@163.com>
2020-07-25 14:19:32 +0800

    Create AbstractLoader.java

---
+++ back/flash-generator/src/main/java/cn/ue/flash/code/AbstractLoader.java
@@ -0,0 +1,22 @@
+package cn.ue.flash.code;
+
+import org.nutz.ioc.Ioc;
+
+import java.util.Map;
+
+/**
+ * @Description: 基础的数据结构加载器
+ * @Author: LCF
+ * @Date: 2020/7/20 15:58
+ * @Package: cn.ue.flash.code
+ * @Version 1.0
+ */
+
+public abstract class AbstractLoader {
+
+    public abstract Map<String, TableDescriptor> loadTables(Ioc ioc,
+                                                            String basePackageName, String baseUri,
+                                                            String servPackageName,
+                                                            String repositoryPackageName,
+                                                            String modPackageName) throws Exception;
+}
