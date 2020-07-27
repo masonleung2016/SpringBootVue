15035d265eead3edc3f5c680d76260a1e3a2cf26
masonleung2016 <masonleung@163.com>
2020-07-28 00:30:02 +0800

    Create BaseCache.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/cache/BaseCache.java
@@ -0,0 +1,19 @@
+package cn.ue.flash.cache;
+
+import cn.ue.flash.bean.vo.SpringContextHolder;
+import cn.ue.flash.service.system.impl.ConstantFactory;
+
+/**
+ * @Description:
+ * @Author: LCF
+ * @Date: 2020/7/20 14:39
+ * @Package: cn.ue.flash.cache.impl
+ * @Version 1.0
+ */
+
+public abstract class BaseCache implements Cache {
+  @Override
+  public void cache() {
+    SpringContextHolder.getBean(ConstantFactory.class).cleanLocalCache();
+  }
+}
