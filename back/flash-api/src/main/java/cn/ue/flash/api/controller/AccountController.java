27ac2423d8b22981f202b20c942ef8ab435b3569
masonleung2016 <masonleung@163.com>
2020-07-27 23:36:04 +0800

    Add files via upload

--- back/flash-api/src/main/java/cn/ue/flash/api/controller/AccountController.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/AccountController.java
@@ -37,102 +37,102 @@ import java.util.Map;
 @RestController
 @RequestMapping("/account")
 public class AccountController extends BaseController {
-    private Logger logger = LoggerFactory.getLogger(AccountController.class);
+  private Logger logger = LoggerFactory.getLogger(AccountController.class);
 
-    @Autowired
-    @Lazy
-    private UserService userService;
+  @Autowired
+  @Lazy
+  private UserService userService;
 
-    /**
-     * 用户登录<br>
-     * 1，验证没有注册<br>
-     * 2，验证密码错误<br>
-     * 3，登录成功
-     *
-     * @param userName
-     * @param password
-     * @return
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Object login(@RequestParam("username") String userName,
-                        @RequestParam("password") String password) {
-        try {
-            //1,
-            User user = userService.findByAccount(userName);
-            if (user == null) {
-                return Rets.failure("用户名或密码错误");
-            }
-            String passwdMd5 = MD5.md5(password, user.getSalt());
-            //2,
-            if (!user.getPassword().equals(passwdMd5)) {
-                return Rets.failure("用户名或密码错误");
-            }
-            if (StringUtil.isEmpty(user.getRoleid())) {
-                return Rets.failure("该用户未配置权限");
-            }
-            String token = userService.loginForToken(user);
-            Map<String, String> result = new HashMap<>(1);
-            result.put("token", token);
-            LogManager.me().executeLog(LogTaskFactory.loginLog(user.getId(), HttpUtil.getIp()));
-            return Rets.success(result);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        return Rets.failure("登录时失败");
+  /**
+   * 用户登录<br>
+   * 1，验证没有注册<br>
+   * 2，验证密码错误<br>
+   * 3，登录成功
+   *
+   * @param userName
+   * @param password
+   * @return
+   */
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public Object login(@RequestParam("username") String userName,
+                      @RequestParam("password") String password) {
+    try {
+      //1,
+      User user = userService.findByAccount(userName);
+      if (user == null) {
+        return Rets.failure("用户名或密码错误");
+      }
+      String passwdMd5 = MD5.md5(password, user.getSalt());
+      //2,
+      if (!user.getPassword().equals(passwdMd5)) {
+        return Rets.failure("用户名或密码错误");
+      }
+      if (StringUtil.isEmpty(user.getRoleid())) {
+        return Rets.failure("该用户未配置权限");
+      }
+      String token = userService.loginForToken(user);
+      Map<String, String> result = new HashMap<>(1);
+      result.put("token", token);
+      LogManager.me().executeLog(LogTaskFactory.loginLog(user.getId(), HttpUtil.getIp()));
+      return Rets.success(result);
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
     }
+    return Rets.failure("登录时失败");
+  }
 
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
-    public Object info() {
-        HttpServletRequest request = HttpUtil.getRequest();
-        Long idUser = null;
-        try {
-            idUser = getIdUser(request);
-        } catch (Exception e) {
-            return Rets.expire();
-        }
-        if (idUser != null) {
-            User user = userService.get(idUser);
-            if (StringUtil.isEmpty(user.getRoleid())) {
-                return Rets.failure("该用户未配置权限");
-            }
-            ShiroUser shiroUser = ShiroFactroy.me().shiroUser(user);
-            Map map = Maps.newHashMap("name", user.getName(), "role", "admin", "roles", shiroUser.getRoleCodes());
-            map.put("permissions", shiroUser.getUrls());
-            Map profile = (Map) Mapl.toMaplist(user);
-            profile.put("dept", shiroUser.getDeptName());
-            profile.put("roles", shiroUser.getRoleNames());
-            map.put("profile", profile);
-
-            return Rets.success(map);
-        }
-        return Rets.failure("获取用户信息失败");
+  @RequestMapping(value = "/info", method = RequestMethod.GET)
+  public Object info() {
+    HttpServletRequest request = HttpUtil.getRequest();
+    Long idUser = null;
+    try {
+      idUser = getIdUser(request);
+    } catch (Exception e) {
+      return Rets.expire();
     }
+    if (idUser != null) {
+      User user = userService.get(idUser);
+      if (StringUtil.isEmpty(user.getRoleid())) {
+        return Rets.failure("该用户未配置权限");
+      }
+      ShiroUser shiroUser = ShiroFactroy.me().shiroUser(user);
+      Map map = Maps.newHashMap("name", user.getName(), "role", "admin", "roles", shiroUser.getRoleCodes());
+      map.put("permissions", shiroUser.getUrls());
+      Map profile = (Map) Mapl.toMaplist(user);
+      profile.put("dept", shiroUser.getDeptName());
+      profile.put("roles", shiroUser.getRoleNames());
+      map.put("profile", profile);
 
-    @RequestMapping(value = "/updatePwd", method = RequestMethod.POST)
-    public Object updatePwd(String oldPassword, String password, String rePassword) {
-        try {
-
-            if (StringUtil.isEmpty(password) || StringUtil.isEmpty(rePassword)) {
-                return Rets.failure("密码不能为空");
-            }
-            if (!password.equals(rePassword)) {
-                return Rets.failure("新密码前后不一致");
-            }
-            User user = userService.get(getIdUser(HttpUtil.getRequest()));
-            if (ApiConstants.ADMIN_ACCOUNT.equals(user.getAccount())) {
-                return Rets.failure("不能修改超级管理员密码");
-            }
-            if (!MD5.md5(oldPassword, user.getSalt()).equals(user.getPassword())) {
-                return Rets.failure("旧密码输入错误");
-            }
-
-            user.setPassword(MD5.md5(password, user.getSalt()));
-            userService.update(user);
-            return Rets.success();
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        return Rets.failure("更改密码失败");
+      return Rets.success(map);
     }
+    return Rets.failure("获取用户信息失败");
+  }
+
+  @RequestMapping(value = "/updatePwd", method = RequestMethod.POST)
+  public Object updatePwd(String oldPassword, String password, String rePassword) {
+    try {
+
+      if (StringUtil.isEmpty(password) || StringUtil.isEmpty(rePassword)) {
+        return Rets.failure("密码不能为空");
+      }
+      if (!password.equals(rePassword)) {
+        return Rets.failure("新密码前后不一致");
+      }
+      User user = userService.get(getIdUser(HttpUtil.getRequest()));
+      if (ApiConstants.ADMIN_ACCOUNT.equals(user.getAccount())) {
+        return Rets.failure("不能修改超级管理员密码");
+      }
+      if (!MD5.md5(oldPassword, user.getSalt()).equals(user.getPassword())) {
+        return Rets.failure("旧密码输入错误");
+      }
+
+      user.setPassword(MD5.md5(password, user.getSalt()));
+      userService.update(user);
+      return Rets.success();
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
+    }
+    return Rets.failure("更改密码失败");
+  }
 
 }
--- back/flash-api/src/main/java/cn/ue/flash/api/controller/BaseController.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/BaseController.java
@@ -23,102 +23,102 @@ import java.net.URLDecoder;
  */
 
 public class BaseController {
-    private static Logger logger = LoggerFactory.getLogger(BaseController.class);
+  private static Logger logger = LoggerFactory.getLogger(BaseController.class);
 
-    /**
-     * 根据token获取用户id，如果不存在则抛出异常
-     *
-     * @param request
-     * @return
-     */
-    public Long getIdUser(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
+  /**
+   * 根据token获取用户id，如果不存在则抛出异常
+   *
+   * @param request
+   * @return
+   */
+  public Long getIdUser(HttpServletRequest request) {
+    String token = request.getHeader("Authorization");
 
-        Long idUser = JwtUtil.getUserId(token);
-        if (idUser == null) {
-            throw new RuntimeException("用户不存在");
-        }
-        return idUser;
+    Long idUser = JwtUtil.getUserId(token);
+    if (idUser == null) {
+      throw new RuntimeException("用户不存在");
+    }
+    return idUser;
+  }
+
+  /**
+   * 获取客户端token
+   *
+   * @param request
+   * @return
+   */
+  public String getToken(HttpServletRequest request) {
+    return request.getHeader("Authorization");
+  }
+
+  public String getToken() {
+    return HttpUtil.getRequest().getHeader("Authorization");
+  }
+
+  /**
+   * 获取客户端ip
+   *
+   * @param req
+   * @return
+   */
+  public String getRealIp(HttpServletRequest req) {
+    String ip = req.getHeader("x-forwarded-for");
+
+    if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = req.getHeader("Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = req.getHeader("WL-Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = req.getRemoteAddr();
+    }
+    if (ip == null || ip.length() == 0 || ApiConstants.IPV6_LOCALHOST.equals(ip)) {
+      ip = ApiConstants.IPV4_LOCALHOST;
+    }
+    return ip;
+  }
+
+  /**
+   * 获取前端传递过来的json字符串<br>
+   * 如果前端使用axios的data方式传参则使用改方法接收参数
+   *
+   * @return
+   */
+  public String getjsonReq() {
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(HttpUtil.getRequest().getInputStream()));
+      String line = null;
+      StringBuilder sb = new StringBuilder();
+      while ((line = br.readLine()) != null) {
+        sb.append(line);
+
+      }
+      br.close();
+      if (sb.length() < 1) {
+        return "";
+      }
+      String reqBody = URLDecoder.decode(sb.toString(), "UTF-8");
+      reqBody = reqBody.substring(reqBody.indexOf("{"));
+      return reqBody;
+
+    } catch (IOException e) {
+
+      logger.error("获取json参数错误！{}", e.getMessage());
+
+      return "";
+
     }
 
-    /**
-     * 获取客户端token
-     *
-     * @param request
-     * @return
-     */
-    public String getToken(HttpServletRequest request) {
-        return request.getHeader("Authorization");
+  }
+
+  public <T> T getFromJson(Class<T> klass) {
+    String jsonStr = getjsonReq();
+    if (StringUtil.isEmpty(jsonStr)) {
+      return null;
     }
-
-    public String getToken() {
-        return HttpUtil.getRequest().getHeader("Authorization");
-    }
-
-    /**
-     * 获取客户端ip
-     *
-     * @param req
-     * @return
-     */
-    public String getRealIp(HttpServletRequest req) {
-        String ip = req.getHeader("x-forwarded-for");
-
-        if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = req.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = req.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || ApiConstants.IP_UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = req.getRemoteAddr();
-        }
-        if (ip == null || ip.length() == 0 || ApiConstants.IPV6_LOCALHOST.equals(ip)) {
-            ip = ApiConstants.IPV4_LOCALHOST;
-        }
-        return ip;
-    }
-
-    /**
-     * 获取前端传递过来的json字符串<br>
-     * 如果前端使用axios的data方式传参则使用改方法接收参数
-     *
-     * @return
-     */
-    public String getjsonReq() {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(HttpUtil.getRequest().getInputStream()));
-            String line = null;
-            StringBuilder sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-
-            }
-            br.close();
-            if (sb.length() < 1) {
-                return "";
-            }
-            String reqBody = URLDecoder.decode(sb.toString(), "UTF-8");
-            reqBody = reqBody.substring(reqBody.indexOf("{"));
-            return reqBody;
-
-        } catch (IOException e) {
-
-            logger.error("获取json参数错误！{}", e.getMessage());
-
-            return "";
-
-        }
-
-    }
-
-    public <T> T getFromJson(Class<T> klass) {
-        String jsonStr = getjsonReq();
-        if (StringUtil.isEmpty(jsonStr)) {
-            return null;
-        }
-        return JsonUtil.fromJson(klass, jsonStr);
-    }
+    return JsonUtil.fromJson(klass, jsonStr);
+  }
 
 
 }
