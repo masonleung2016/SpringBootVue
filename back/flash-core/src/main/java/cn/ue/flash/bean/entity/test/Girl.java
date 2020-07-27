5d3bde03db37b0dcb3d4c62164bd018a633dd14d
masonleung2016 <masonleung@163.com>
2020-07-28 00:23:01 +0800

    Create Girl.java

---
+++ back/flash-core/src/main/java/cn/ue/flash/bean/entity/test/Girl.java
@@ -0,0 +1,467 @@
+package cn.ue.flash.bean.entity.test;
+
+import cn.ue.flash.bean.entity.BaseEntity;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * @Description: 该实体用于测试生成代码
+ * @Author: LCF
+ * @Date: 2020/7/20 14:07
+ * @Package: cn.ue.flash.bean.entity.test
+ * @Version 1.0
+ */
+
+@Entity(name = "t_test_girl")
+@Table(appliesTo = "t_test_girl", comment = "女孩")
+public class Girl extends BaseEntity {
+  @Column(columnDefinition = "VARCHAR(32) COMMENT '姓名'")
+  private String name;
+  @Column(columnDefinition = "INT COMMENT '年龄'")
+  private Integer age;
+  @Column(columnDefinition = "DATE COMMENT '生日'")
+  private Date birthday;
+  @Column(name = "has_boy_friend", columnDefinition = "TINYINT COMMENT '是否有男朋友'")
+  private Boolean hasBoyFriend;
+
+  @Override
+  public String getOriginalFileName() {
+    return null;
+  }
+
+  @Override
+  public void setOriginalFileName(String originalFileName) {
+
+  }
+
+  @Override
+  public String getAblatePath() {
+    return null;
+  }
+
+  @Override
+  public void setAblatePath(String s) {
+
+  }
+
+  @Override
+  public String getRealFileName() {
+    return null;
+  }
+
+  @Override
+  public void setRealFileName(String name) {
+
+  }
+
+  @Override
+  public String getSalt() {
+    return null;
+  }
+
+  @Override
+  public byte[] getPassword() {
+    return new byte[0];
+  }
+
+  @Override
+  public void setPassword(String md5) {
+
+  }
+
+  @Override
+  public String getRoleid() {
+    return null;
+  }
+
+  @Override
+  public void setRoleid(String roleIds) {
+
+  }
+
+  @Override
+  public Long getId() {
+    return null;
+  }
+
+  @Override
+  public Object getName() {
+    return null;
+  }
+
+  @Override
+  public void setName(Object name) {
+
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String getAccount() {
+    return null;
+  }
+
+  @Override
+  public void setAccount(String admin) {
+
+  }
+
+  @Override
+  public Object getCfgName() {
+    return null;
+  }
+
+  @Override
+  public void setCfgName(Object cfgName) {
+
+  }
+
+  @Override
+  public Object getCfgValue() {
+    return null;
+  }
+
+  @Override
+  public void setCfgValue(Object cfgValue) {
+
+  }
+
+  @Override
+  public Object getCfgDesc() {
+    return null;
+  }
+
+  @Override
+  public void setCfgDesc(Object cfgDesc) {
+
+  }
+
+  @Override
+  public Object getAuthor() {
+    return null;
+  }
+
+  @Override
+  public void setAuthor(Object author) {
+
+  }
+
+  @Override
+  public Object getContent() {
+    return null;
+  }
+
+  @Override
+  public void setContent(Object content) {
+
+  }
+
+  @Override
+  public Object getIdChannel() {
+    return null;
+  }
+
+  @Override
+  public void setIdChannel(Object idChannel) {
+
+  }
+
+  @Override
+  public Object getImg() {
+    return null;
+  }
+
+  @Override
+  public void setImg(Object img) {
+
+  }
+
+  @Override
+  public Object getTitle() {
+    return null;
+  }
+
+  @Override
+  public void setTitle(Object title) {
+
+  }
+
+  @Override
+  public Object getSimplename() {
+    return null;
+  }
+
+  @Override
+  public void setSimplename(Object simplename) {
+
+  }
+
+  @Override
+  public int getPid() {
+    return 0;
+  }
+
+  @Override
+  public void setPid(Object pid) {
+
+  }
+
+  @Override
+  public Object getFullname() {
+    return null;
+  }
+
+  @Override
+  public void setFullname(Object fullname) {
+
+  }
+
+  @Override
+  public Object getNum() {
+    return null;
+  }
+
+  @Override
+  public void setNum(Object num) {
+
+  }
+
+  @Override
+  public Object getTips() {
+    return null;
+  }
+
+  @Override
+  public void setTips(Object tips) {
+
+  }
+
+  @Override
+  public String getCode() {
+    return null;
+  }
+
+  @Override
+  public Object getCron() {
+    return null;
+  }
+
+  @Override
+  public void setCron(Object cron) {
+
+  }
+
+  @Override
+  public Object getJobClass() {
+    return null;
+  }
+
+  @Override
+  public void setJobClass(Object jobClass) {
+
+  }
+
+  @Override
+  public Object getNote() {
+    return null;
+  }
+
+  @Override
+  public void setNote(Object note) {
+
+  }
+
+  @Override
+  public Object getData() {
+    return null;
+  }
+
+  @Override
+  public void setData(Object data) {
+
+  }
+
+  @Override
+  public int getStatus() {
+    return 0;
+  }
+
+  @Override
+  public void setStatus(int code) {
+
+  }
+
+  @Override
+  public void setSex(Object sex) {
+
+  }
+
+  @Override
+  public void setPhone(Object phone) {
+
+  }
+
+  @Override
+  public void setEmail(Object email) {
+
+  }
+
+  @Override
+  public String getCreateTime() {
+    return null;
+  }
+
+  @Override
+  public void setCreateTime(Date date) {
+
+  }
+
+  @Override
+  public void setType(Integer type) {
+
+  }
+
+  @Override
+  public void setState(int i) {
+
+  }
+
+  @Override
+  public void setReceiver(String receiver) {
+
+  }
+
+  @Override
+  public String getClassName() {
+    return null;
+  }
+
+  @Override
+  public String getTplCode() {
+    return null;
+  }
+
+  @Override
+  public void setTplCode(String tplCode) {
+
+  }
+
+  @Override
+  public String getPids() {
+    return null;
+  }
+
+  @Override
+  public void setPids(String s) {
+
+  }
+
+  @Override
+  public String getPcode() {
+    return null;
+  }
+
+  @Override
+  public void setPcode(String s) {
+
+  }
+
+  @Override
+  public Integer getLevels() {
+    return null;
+  }
+
+  @Override
+  public void setLevels(int i) {
+
+  }
+
+  @Override
+  public String getPcodes() {
+    return null;
+  }
+
+  @Override
+  public void setPcodes(String s) {
+
+  }
+
+  @Override
+  public void setExecResult(String exeResult) {
+
+  }
+
+  @Override
+  public void setExecAt(Date exeAt) {
+
+  }
+
+  @Override
+  public String getJobGroup() {
+    return null;
+  }
+
+  @Override
+  public Object isConcurrent() {
+    return null;
+  }
+
+  @Override
+  public boolean isDisabled() {
+    return false;
+  }
+
+  @Override
+  public void setDisabled(boolean b) {
+
+  }
+
+  @Override
+  public Long getDeptid() {
+    return null;
+  }
+
+  @Override
+  public void setDeptid(Object deptid) {
+
+  }
+
+  @Override
+  public void setCreateBy(Long userId) {
+
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Date getBirthday() {
+    return birthday;
+  }
+
+  @Override
+  public void setBirthday(Object birthday) {
+
+  }
+
+  public void setBirthday(Date birthday) {
+    this.birthday = birthday;
+  }
+
+  public Boolean getHasBoyFriend() {
+    return hasBoyFriend;
+  }
+
+  public void setHasBoyFriend(Boolean hasBoyFriend) {
+    this.hasBoyFriend = hasBoyFriend;
+  }
+}
