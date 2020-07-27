0bec5a3fca111b5db195d1886798a77c58c5ce49
masonleung2016 <masonleung@163.com>
2020-07-27 23:59:17 +0800

    Add files via upload

--- back/flash-core/src/main/java/cn/ue/flash/bean/constant/factory/PageFactory.java
+++ back/flash-core/src/main/java/cn/ue/flash/bean/constant/factory/PageFactory.java
@@ -19,37 +19,37 @@ import javax.servlet.http.HttpServletRequest;
 
 public class PageFactory<T> {
 
-    public Page<T> defaultPage() {
-        HttpServletRequest request = HttpUtil.getRequest();
-        /**每页多少条数据*/
-        int limit = Integer.valueOf(request.getParameter("limit"));
-        String pageNum = request.getParameter("page");
-        /**每页的偏移量(本页当前有多少条)*/
-        int offset = 0;
-        if (StringUtils.isNotEmpty(pageNum)) {
-            offset = (Integer.valueOf(pageNum) - 1) * limit;
-        } else {
+  public Page<T> defaultPage() {
+    HttpServletRequest request = HttpUtil.getRequest();
+    /**每页多少条数据*/
+    int limit = Integer.valueOf(request.getParameter("limit"));
+    String pageNum = request.getParameter("page");
+    /**每页的偏移量(本页当前有多少条)*/
+    int offset = 0;
+    if (StringUtils.isNotEmpty(pageNum)) {
+      offset = (Integer.valueOf(pageNum) - 1) * limit;
+    } else {
 
-            offset = Integer.valueOf(request.getParameter("offset"));
-        }
-        /**排序字段名称*/
-        String sortName = request.getParameter("sort");
-        /**asc或desc(升序或降序)*/
-        String order = request.getParameter("order");
-        if (StringUtil.isEmpty(sortName)) {
-            Page<T> page = new Page<>((offset / limit + 1), limit);
-            return page;
-        } else {
-            Page<T> page = new Page<>((offset / limit + 1), limit, sortName);
-            if (Order.ASC.getDes().equals(order)) {
-                Sort sort = Sort.by(Sort.Direction.ASC, sortName);
-                page.setSort(sort);
-            } else {
-                Sort sort = Sort.by(Sort.Direction.DESC, sortName);
-                page.setSort(sort);
-
-            }
-            return page;
-        }
+      offset = Integer.valueOf(request.getParameter("offset"));
     }
+    /**排序字段名称*/
+    String sortName = request.getParameter("sort");
+    /**asc或desc(升序或降序)*/
+    String order = request.getParameter("order");
+    if (StringUtil.isEmpty(sortName)) {
+      Page<T> page = new Page<>((offset / limit + 1), limit);
+      return page;
+    } else {
+      Page<T> page = new Page<>((offset / limit + 1), limit, sortName);
+      if (Order.ASC.getDes().equals(order)) {
+        Sort sort = Sort.by(Sort.Direction.ASC, sortName);
+        page.setSort(sort);
+      } else {
+        Sort sort = Sort.by(Sort.Direction.DESC, sortName);
+        page.setSort(sort);
+
+      }
+      return page;
+    }
+  }
 }
