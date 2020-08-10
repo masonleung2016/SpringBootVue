37a230b1b816ed158b8bc9012d6fd22c4ef9b858
masonleung2016 <masonleung@163.com>
2020-08-10 19:25:41 +0800

    Update Contact.java

--- back/flash-api/src/main/java/cn/ue/flash/api/controller/front/officialsite/Contact.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/front/officialsite/Contact.java
@@ -14,15 +14,53 @@ import java.util.Date;
 
 @Data
 public class Contact {
-  
-  public String username;
-  
-  private String email;
-  
-  private String mobile;
-  
-  private String description;
-  
-  private Date createAt;
-  
+    public String username;
+
+    private String email;
+
+    private String mobile;
+
+    private String description;
+
+    private Date createAt;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
 }
