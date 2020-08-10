b43bec74c091035bdb0bc2e0b1fd1249ceb0b4e4
masonleung2016 <masonleung@163.com>
2020-08-10 18:13:44 +0800

    Update ContactsController.java

--- back/flash-api/src/main/java/cn/ue/flash/api/controller/cms/ContactsController.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/cms/ContactsController.java
@@ -9,6 +9,7 @@ import cn.ue.flash.bean.vo.query.SearchFilter;
 import cn.ue.flash.service.cms.ContactsService;
 import cn.ue.flash.utils.DateUtil;
 import cn.ue.flash.utils.factory.Page;
+
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
