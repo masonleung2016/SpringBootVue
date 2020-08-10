a13fbaaddb4916a65cd3ba021e05abb0437231a3
masonleung2016 <masonleung@163.com>
2020-08-10 19:21:37 +0800

    Update Contact.java

--- back/flash-api/src/main/java/cn/ue/flash/api/controller/front/officialsite/Contact.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/front/officialsite/Contact.java
@@ -14,9 +14,15 @@ import java.util.Date;
 
 @Data
 public class Contact {
+  
   public String username;
+  
   private String email;
+  
   private String mobile;
+  
   private String description;
+  
   private Date createAt;
+  
 }
