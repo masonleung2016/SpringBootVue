b989896e20a0368a9efe43194522777e51188159
masonleung2016 <masonleung@163.com>
2020-08-15 18:02:23 +0800

    Update MenuController.java

--- back/flash-api/src/main/java/cn/ue/flash/api/controller/system/MenuController.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/controller/system/MenuController.java
@@ -17,6 +17,7 @@ import cn.ue.flash.service.system.impl.ConstantFactory;
 import cn.ue.flash.utils.HttpUtil;
 import cn.ue.flash.utils.Maps;
 import cn.ue.flash.utils.StringUtil;
+
 import com.google.common.collect.Lists;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
