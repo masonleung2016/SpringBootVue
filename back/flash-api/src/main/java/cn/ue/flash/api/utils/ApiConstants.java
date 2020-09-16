6bea6096a30ff335cd46ecc70777c1af88c0bf08
masonleung2016 <masonleung@163.com>
2020-09-16 11:03:02 +0800

    Update ApiConstants.java

--- back/flash-api/src/main/java/cn/ue/flash/api/utils/ApiConstants.java
+++ back/flash-api/src/main/java/cn/ue/flash/api/utils/ApiConstants.java
@@ -9,6 +9,7 @@ package cn.ue.flash.api.utils;
  */
 
 public class ApiConstants {
+  
   public static final String HTTP_METHOD_OPTIONS = "OPTIONS";
 
   public static final String ADMIN_ACCOUNT = "admin";
@@ -18,4 +19,5 @@ public class ApiConstants {
   public static final String IPV6_LOCALHOST = "0:0:0:0:0:0:0:1";
 
   public static final String IPV4_LOCALHOST = "127.0.0.1";
+  
 }
